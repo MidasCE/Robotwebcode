8e24e3d845eca36c5da68cbb762e96f6e365817b
MidasCE <midaskmitl@gmail.com>
2013-09-06 11:32:48 +0700

    6/9/2556
    อับเดตหน้า ibot โหลดใบสมัคร พร้อรายละเอียด
    
    เริ่มทำหน้า image galley อบ่างง่ายๆ แต่ยังไม่สมบูรณ์

---
+++ body/body_gallery.php
@@ -0,0 +1,48 @@
+<div class="container">
+<div class="row-fluid">
+<img src="img/gallery.png" width="940" height="50">
+<div id="container" style="position: relative; height: 612px;" class="masonry">
+      <div class="item well well-small masonry-brick" style="position: absolute; top: 0px; left: 0px;">
+			<a href="img/Gallery/ibotcamp/(1).jpg">
+            <img src="img/Gallery/ibotcamp/(1).jpg"></a>
+      </div>
+      <div class="item well well-small masonry-brick" style="position: absolute; top: 0px; left: 290px;">
+			<a href="img/Gallery/ibotcamp/(2).jpg">
+            <img src="img/Gallery/ibotcamp/(2).jpg"></a>
+      </div>
+      <div class="item well well-small masonry-brick" style="position: absolute; top: 0px; left: 580px;">
+			<a href="img/Gallery/ibotcamp/(3).jpg">
+            <img src="img/Gallery/ibotcamp/(3).jpg"></a>
+      </div>
+      <div class="item well well-small masonry-brick" style="position: absolute; top: 204px; left: 0px;">
+			<a href="img/Gallery/ibotcamp/(4).jpg">
+            <img src="img/Gallery/ibotcamp/(4).jpg"></a>
+      </div>
+      <div class="item well well-small masonry-brick" style="position: absolute; top: 204px; left: 290px;">
+			<a href="img/Gallery/ibotcamp/(5).jpg">
+            <img src="img/Gallery/ibotcamp/(5).jpg"></a>
+      </div>
+      <div class="item well well-small masonry-brick" style="position: absolute; top: 204px; left: 580px;">
+			<a href="img/Gallery/ibotcamp/(6).jpg">
+            <img src="img/Gallery/ibotcamp/(6).jpg"></a>
+      </div>
+      <div class="item well well-small masonry-brick" style="position: absolute; top: 408px; left: 0px;">
+			<a href="img/Gallery/ibotcamp/(7).jpg">
+            <img src="img/Gallery/ibotcamp/(7).jpg"></a>
+      </div>
+      <div class="item well well-small masonry-brick" style="position: absolute; top: 408px; left: 290px;">
+			<a href="img/Gallery/ibotcamp/(8).jpg">
+            <img src="img/Gallery/ibotcamp/(8).jpg"></a>
+      </div>
+</div>
+</div>
+</div>
+
+<p>
+<link rel="stylesheet" href="css/gallery.css">
+<script src="js/masonry.min.js"></script>
+  <script src="http://code.jquery.com/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script></p>
+<p>&nbsp;</p>
+</body>
+</html>
--- body/body_ibot.php
+++ body/body_ibot.php
@@ -1,9 +1,88 @@
 <div class="container">
-	<img src="img/i-bot ads.JPG"> 
-	<hr>
-	<h1><center><font color = "FFA302">I - BOT ! ต้องการคุณ!!</font></center></h1><br>
-	<h3><font color = "000000">ตอนนี้เรายังต้อการน้องๆมาร่วมกันทำค่ายอีกเยอะเลย สนใจติดต่อได้เลยนะครับ :)</font></h3>
+  <div>
+    		<span class="span8">
+        		<div id="myCarousel" class="carousel slide">
+                <ol class="carousel-indicators">
+                  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                  <li data-target="#myCarousel" data-slide-to="1" class=""></li>
+                  <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+                </ol>
+                <div class="carousel-inner">
+                  <div class="item active">
+                  	<img src="img/Gallery/ibotcamp/ibot_2.jpg">
+                    <div class="carousel-caption">
+                      	<h4>ความรู้</h4>
+                      	<p>เรียนรู้พื้นฐานการทำหุ่นยนต์ พร้อมลงมือทำของจริง!</p>
+                    </div>
+                  </div>
+                  <div class="item">
+                    <img src="img/Gallery/ibotcamp/ibot_3.jpg">
+                    <div class="carousel-caption">
+               	 	  <h4>สนุก</h4>
+                      	<p>สนุกไปกับกิจกรรมจากพี่ๆวิศวลาดกระบัง!</p>
+                    </div>
+                  </div>
+                  <div class="item">
+                    <img src="img/Gallery/ibotcamp/ibot_1.jpg">
+                    <div class="carousel-caption">
+                      	<h4>ประทับใจ</h4>
+                      	<p>รู้จักเพื่อนๆ พี่ๆใหม่ๆ ประสบการณ์ดีๆอีกเพียบ!</p>
+                    </div>
+                  </div>
+                </div>
+                
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
+              </div>
+			</span>
+   		</div>
+		<div>
+			<span class="span3">
+				<div class="alert alert-info">
+                	<center>
+                	  <img src="img/Gallery/ibotcamp/poster.png" width="220" height="350" />
+                      <br></br>
+                      <br></br>
+                      <img src="img/RobotSmile(2).png" width="290" height="220" />
+                      <br></br>
+                    </center>
+					<h3 style="font-family:Tahoma, Geneva, sans-serif;"><center>เปิดรับสมัครตั้งแต่วันนี้ - 27 กันยายน 2556</center> </h3>
+       		</div>
+			</span>
+   		</div>
 </div>
+<div class="container">
+	<div class="alert">
+        <h2 style="font-family:Tahoma, Geneva, sans-serif;">รายละเอียดโครงการ<br></h2>    <p>เป็นค่ายที่นำน้องมัธยมปลายทั่วประเทศเข้ามาเรียนรู้ถึงศาสตร์ต่างๆในการทำหุ่นยนต์ รวมไปถึงการแนะแนวถึงการเรียนวิศวะว่าเป็นประมาณไหน
+        <br>น้องจะได้เห็นภาพการทำงานจริงจากการเข้าค่าย ค่ายนี้เป็นค่ายแบบค้างคืน ตั้งแต่วันที่ 13 - 17 ตุลาคม 2556 มีพี่ๆดูแลตลอด 24 ชั่วโมง</p>
+        <a href="#myModal" role="button" class="btn" data-toggle="modal">Poster</a>
+ 
+<!-- Modal -->
+<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+    <h3 id="myModalLabel">I - BOT 6</h3>
+  </div>
+  <div class="modal-body">
+    <p><img src="img/ibot_poster.jpg" width="679" height="960" /></p>
+  </div>
+  <div class="modal-footer">
+    <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+  </div>
+</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="alert alert-success">
+		<a href="file/ibot6.rar" class="btn btn-primary btn-large">
+      		Download ใบสมัคร !!
+    	</a>
+        <h3 style="font-family:Tahoma, Geneva, sans-serif;">ส่งใบสมัครมาที่ <br>
+        ชุมนุมโรบอท คณะวิศวกรรมศาสตร์ สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง ตู้ ปณ.100 ปณศ. ลาดกระบัง กรุงเทพ 10520</h3>
+	</div>
+   </div>
+
 <p>
   <script src="http://code.jquery.com/jquery.js"></script>
   <script src="js/bootstrap.min.js"></script></p>
